1eb93390e5c7edbac4b2b8fd2a2e7ec8e23d1b83
Arete929 <maranatha929@gmail.com>
2026-08-02 23:47:45 +0900

    교사 PWA 추가(배너제거) + 탭 순서 오늘/누적/순위 + 서포터 5452 예시 삭제 + SW v1.2.0

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 아침걷기 챌린지 PWA 서비스워커 | 버전 올리면 캐시 갱신·자동 새로고침 */
-var CACHE = 'achim-v1.1.0';
-var ASSETS = ['./', './index.html', './supporter.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+var CACHE = 'achim-v1.2.0';
+var ASSETS = ['./', './index.html', './supporter.html', './teacher.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }).then(function () { return self.skipWaiting(); }));
